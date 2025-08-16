5a4a8b94f026f0dff635fc45237ce686c946b6e6
duxdudu <dudufredu@gmail.com>
2025-08-16 20:03:06 +0200

    this about connect backend and frontend

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000', 
     'http://127.0.0.1:3000',
-    'https://teofly-photography.onrender.com'
+    'https://teofly-photography.onrender.com',
+    'https://teoflys-frontend.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
